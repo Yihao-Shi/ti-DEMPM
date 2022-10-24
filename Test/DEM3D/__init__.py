e86f6bab1d56b14c4e31d5caf788633a4382c2c7
Yihao-Shi <91011509+Yihao-Shi@users.noreply.github.com>
2022-10-24 22:15:58 +0800

    Delete Test directory

--- Test/DEM3D/__init__.py
+++
@@ -1,4 +0,0 @@
-import taichi as ti
-import sys, os
-sys.path.append(os.environ.get('tiDEMPM'))
-
--- Test/MPM2D/__init__.py
+++
@@ -1,4 +0,0 @@
-import taichi as ti
-import sys
-sys.path.append("/home/eleven/Work/DEMPMCoupling/Library")
-
